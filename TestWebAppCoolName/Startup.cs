de36d9c4b883b4e6f125bafa2ae4b9caabe47208
Jan Fujdiar <jan.fujdiar@tescosw.cz>
2019-05-24 14:51:43 +0200

    hangfire for repeatly calling websites to prevent iis server to sleep

--- TestWebAppCoolName/Startup.cs
+++ TestWebAppCoolName/Startup.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.Owin;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Hangfire;
+using Hangfire.Dashboard;
+using Microsoft.Owin;
 using Owin;
+using TestWebAppCoolName.Models;
 
 [assembly: OwinStartupAttribute(typeof(TestWebAppCoolName.Startup))]
 namespace TestWebAppCoolName
@@ -9,6 +15,29 @@ namespace TestWebAppCoolName
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            GlobalConfiguration.Configuration.UseSqlServerStorage("DefaultConnection");
+
+            var options = new DashboardOptions
+            {
+                AuthorizationFilters = new[]
+                {
+                    new AuthorizationFilter { Roles = Roles.Admin },
+                    
+                }
+            };
+            app.UseHangfireDashboard("/hangfire", options);
+            app.UseHangfireServer();
+            RecurringJob.AddOrUpdate(() =>
+                GetSite(), Cron.Hourly);
+        }
+
+        public async Task GetSite()
+        {
+            using (var client = new HttpClient())
+            {
+                var result = await client.GetAsync("http://www.vkurzu.cz");
+                Console.WriteLine(result.StatusCode);
+            }
         }
     }
 }
