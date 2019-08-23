203ba567135f8f265818fd5136778ac12d74e403
Jan Fujdiar <jan.fujdiar@tescosw.cz>
2019-08-23 07:48:16 +0200

    User can change own passwvord

--- TestWebAppCoolName/Models/ManageViewModels.cs
+++ TestWebAppCoolName/Models/ManageViewModels.cs
@@ -43,17 +43,17 @@ namespace TestWebAppCoolName.Models
     {
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
+        [Display(Name = "Aktuální heslo")]
         public string OldPassword { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "New password")]
+        [Display(Name = "Nové heslo")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
+        [Display(Name = "Potvrdit nové heslo")]
         [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
