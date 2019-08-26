e7a3edbce8a82b34db7a7b14f134f2869fa15ad7
Jan Fujdiar <jan.fujdiar@tescosw.cz>
2019-08-26 15:32:28 +0200

    Groupování článků podle tagů

---
+++ TestWebAppCoolName/Controllers/TagController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestWebAppCoolName.DAL;
+using TestWebAppCoolName.Models;
+
+namespace TestWebAppCoolName.Controllers
+{
+    public class TagController : Controller
+    {
+        private TagRepository _repo;
+
+        public TagController()
+        {
+            _repo = new TagRepository(new ApplicationDbContext());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _repo.Dispose();
+            base.Dispose(disposing);
+        }
+
+        // GET: Tag
+        [HttpGet]
+        [Route("tag/{tagName}")]
+        public ActionResult Index(string tagName)
+        {
+            if (string.IsNullOrEmpty(tagName))
+            {
+                return HttpNotFound();
+            }
+
+            var articles = _repo.GetArticlesByTagName(tagName);
+            if (articles == null)
+            {
+                return HttpNotFound();
+            }
+            return View(articles);
+        }
+    }
+}
---
+++ TestWebAppCoolName/DAL/TagRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TestWebAppCoolName.Models;
+
+namespace TestWebAppCoolName.DAL
+{
+    public class TagRepository : IDisposable
+    {
+        private ApplicationDbContext _context;
+
+        public TagRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Blog> GetArticlesByTagName(string tagName)
+        {
+            var tag = _context.Tags.Include(a => a.Blogs).FirstOrDefault(x => x.Name == tagName);
+            return tag?.Blogs;
+        }
+        public void Dispose()
+        {
+            _context?.Dispose();
+        }
+    }
+}
